26fe56d01b2a0768af5047ce4519831c7a324c2b
unknown <Breke64@gmail.com>
2023-10-12 10:47:54 -0400

    worked on javascript more before class

---
+++ Assets/js/script.js
@@ -0,0 +1,131 @@
+const questions = [
+  {
+    question: 'Javascript is an _____ language?',
+    choices: ['Object-Orientated','Object-Based','Procedural','None of the above'],
+    answer: 1
+  },
+  {
+    question: 'Which of the following keywords is used to define a variable in Javascript?',
+    choices: ['Var','let','Both A and B','None of the above'],
+    answer: 2
+  },
+]
+
+var timer = 30;
+var currentQ = 0;
+var correctAnswer = 0;
+var quizOver = false;
+
+
+scores = document.getElementById('scores');
+scores.textContent = 'View High Scores!';
+
+time = document.getElementById('time');
+
+
+quizContainer = document.getElementById('quiz');
+quizContainer.textContent = 'JavaScript Quiz!';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var body = document.body;
+// var quizTitle = document.createElement('h1')
+// var header = document.createElement('header');
+// var time = document.createElement('p');
+// var highScores = document.createElement('div');
+// var quizHead = document.createElement('div');
+
+// var quizCont = document.getElementById('quiz-container');
+// var timer = document.getElementById('time');
+// timer = 30;
+// var viewScore = document.getElementById('scores');
+// viewScore.textContent = 'View High Scores!';
+
+
+// var mainDiv = document.getElementById('quiz');
+// mainDiv.textContent = 'Javascript Quiz!';
+// mainDiv.setAttribute('style','background-color: grey;');
+
+// addEventListener('click',startButton);
+
+
+// var highScores = document.getElementById('scores');
+
+// var container = document.getElementById('main');
+// var quiz = document.getElementById('quiz');
+// var questionsDiv = document.getElementById('questions');
+// var olAnswers = document.getElementById('answer-buttons');
+// var btn1 = document.createElement('button');
+
+
+
+
+// timer that starts on btn click event listener for button id 
+
+// presented with question
+
+//when answered wrong time is subtracted from total time
+
+//when all questions are answered or timer runs out game ends
+
+// when game ends then i can save my score -local storage
+
+
+// highscores tab   time   and quiz on page at all times
+
+
+//function for timer
+
+
+//etc....
+
+
+//event listener for start btn
+
+
+
+//event listener for timer
+
+
+
+// var timer = 30;
+// time.textContent = 'Time';
+// header.textContent = 'View High Scores!';
+// header.setAttribute('style','background-color: grey; color: white; border: 2px solid black; border-radius: 10px;')
+// body.appendChild(header);
+// header.appendChild(time);
+
+
+// quizTitle.textContent = 'JavaScript Quiz!';
+// body.appendChild(quizTitle);
+// quizInfo.textContent = 'This is a timed Javascript Quiz where you can test your knowledge and compare your score along with other fellow students.';
+// body.appendChild(quizInfo);
+
+
+
+
